4b5b91f8ff07fbe21425abca216457b5b14fb6c2
Esneider Chavarro <echavarro6@misena.edu.co>
2021-04-18 17:55:07 -0500

    js. rol

---
+++ js/seguridad/rol.js
@@ -0,0 +1,50 @@
+function Enviar(accion,id){
+    if(id==null){
+        id=$('#hidIdRol').val();
+    }
+    var parametros = {
+        "id":id,
+        "descripcion" :$('#txtDescripcion').val(),
+        "estado":$('#cmbEstado').val(),
+        "accion":accion
+    };
+
+     $.ajax({
+            data:  parametros, //datos que se van a enviar al ajax
+            url:   '../../controlador/seguridad/rol.C.php', //archivo php que recibe los datos
+            type:  'post', //método para enviar los datos
+            dataType: 'json',//Recibe el array desde php
+           
+            success:  function (respuesta) { //procesa y devuelve la respuesta
+                
+                //respuesta adicionar
+                if(respuesta['accion']=='ADICIONAR'){
+                    alert(respuesta['respuesta']);
+                }
+                
+                //Respuesta muchos registros
+                if(respuesta['accion']=='CONSULTAR' && respuesta['numeroRegistros']>1){
+                    $("#resultado").html(respuesta['tablaRegistro']);
+                    $('#divEliminar').html(respuesta['eliminar']).hide();
+                }
+
+                //Respuesta un registro
+                if(respuesta['accion']=='CONSULTAR'){
+                    $('#hidIdRol').val(respuesta['id']);
+                    $('#txtDescripcion').val(respuesta['descripcion']);
+                    $('#cmbEstado').html(respuesta['estado']);
+                    $('#divEliminar').html(respuesta['eliminar']);
+                }
+
+                //Respuesta modificar
+                if(respuesta['accion']=='MODIFICAR'){
+                    alert(respuesta['respuesta']);
+                }
+                
+                //Respuesta eliminar
+                if(respuesta['accion']=='ELIMINAR'){
+                    alert(respuesta['respuesta']);
+                }
+            }
+    });
+}

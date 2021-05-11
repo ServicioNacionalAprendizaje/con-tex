ee9de232576372dcc5415caf8b790d11e8e27d98
Oscar Leonardo Perdomo González <olperdomo6@misena.edu.co>
2021-05-10 19:23:42 -0500

    cambios producto

---
+++ busqueda/categoria.B.php
@@ -0,0 +1,19 @@
+<?php
+require '../entorno/conexion.php';
+require '../modelo/produccion/categoria.M.php';
+
+$arrCategoria = array();
+$contador = 0;
+$categoria = new Categoria();
+$categoria->setDescripcion($_REQUEST['term']);
+
+$categoria->Consultar();
+$numeroRegistros = $categoria->conn->obtenerNumeroRegistros();
+while($rowCategoria = $categoria->conn->obtenerObjeto()){
+    $arrCategoria[$contador]['id'] = $rowCategoria->id_categoria;
+    $arrCategoria[$contador]['value'] = $rowCategoria->descripcion;
+
+    $contador++;
+}
+echo json_encode($arrCategoria);
+?>

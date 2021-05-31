b9148432a8622bcf187966fd87c2fd5dbe58e741
Wilmer Vargas Bautista <wvargas08@misena.edu.co>
2021-05-31 14:39:10 -0500

    Iconos en la parte superior del software y dataTable en español

--- js/produccion/categoria.js
+++ js/produccion/categoria.js
@@ -27,9 +27,30 @@ function Enviar(accion,id){
                 //Respuesta muchos registros
                 if(respuesta['accion']=='CONSULTAR' && respuesta['numeroRegistros']>1){
                     $("#resultado").html(respuesta['tablaRegistro']);
-                    $(document).ready( function () {
-                        $('#tableDatos').DataTable();
-                    } );
+                    //Código para DataTable
+
+                //Para inicializar datatable de la manera más simple
+
+                $(document).ready(function() {    
+                    $('#tableDatos').DataTable({
+                    //para cambiar el lenguaje a español
+                        "language": {
+                                "lengthMenu": "Mostrar _MENU_ registros",
+                                "zeroRecords": "No se encontraron resultados",
+                                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                                "sSearch": "Buscar:",
+                                "oPaginate": {
+                                "sFirst": "Primero",
+                                "sLast":"Último",
+                                "sNext":"Siguiente",
+                                "sPrevious": "Anterior"
+                                },
+                                "sProcessing":"Procesando...",
+                            }
+                    });     
+                });
                     //$('#divEliminar').html(respuesta['eliminar']).hide();
                 }
 

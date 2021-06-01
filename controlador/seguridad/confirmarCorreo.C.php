1253062faae133884baa560ca7c38cd6b632cd0d
Eduardo Andres Peña Rojas - SENA <eapena634@misena.edu.co>
2021-06-01 17:26:34 -0500

    Se agrego id y se borró funcion que no se usaba

--- controlador/seguridad/confirmarCorreo.C.php
+++ controlador/seguridad/confirmarCorreo.C.php
@@ -1,14 +1,6 @@
 <?php
 include '../../entorno/conexion.php';
 require '../../modelo/nomina/empleado.M.php';
-function searchForId($id, $array) {
-    foreach ($array as $key => $val) {
-        if ($val['uid'] === $id) {
-            return $key;
-        }
-    }
-    return null;
- }
 $contador = 0;
 $respuesta = array();
 $empleado = new Empleado();
@@ -17,10 +9,13 @@ $numeroRegistros = $empleado->conn->obtenerNumeroRegistros();
 // $registros = $empleado->conn->ObtenerRegistros();
 foreach($empleado->conn->ObtenerRegistros() AS $rowConsulta){
     $registros[$contador] = $rowConsulta[0];
+    $ids[$contador] = $rowConsulta[1];
     $contador++;
 }
 $correo = $_POST['correo'];
 if (in_array($correo, $registros)) {
+    $index = array_search($correo, $registros);
+    $respuesta['id'] = $ids[$index];
     $respuesta['respuesta'] = "Correo confirmado.";
     $respuesta['estado'] = true;
 } else {

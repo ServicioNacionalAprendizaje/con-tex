c41534dbd65c7fd5777aa96b902af7194b918737
Eduardo Andres Peña Rojas - SENA <eapena634@misena.edu.co>
2021-06-01 22:24:34 -0500

    Se le asigno el correo del aplicativo

--- modelo/seguridad/enviarCorreo.M.php
+++ modelo/seguridad/enviarCorreo.M.php
@@ -61,14 +61,14 @@ class enviarCorreo
             $mail->isSMTP(); //Send using SMTP
             $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
             $mail->SMTPAuth = true; //Enable SMTP authentication
-            $mail->Username = 'videoqualityasessment@gmail.com'; //SMTP username
-            $mail->Password = 'W;Q~FzjHqsykIV2ygt\I'; //SMTP password
+            $mail->Username = 'noreplay.contex@gmail.com'; //SMTP username
+            $mail->Password = '%y1Ix;7tJ5;2R?9Ip$8e'; //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port = 587; //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
             // $mail->Port = 465; //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
-            $mail->setFrom('videoqualityasessment@gmail.com', 'Aplicativo Con-Tex');
+            $mail->setFrom('noreplay.contex@gmail.com', 'Aplicativo Con-Tex');
             $mail->addAddress($this->correo); //Add a recipient
 
             //Content

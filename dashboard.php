9a42eed26570dfefcda29b0beb8a8f5df28e7b9c
Wilmer Vargas Bautista <wvargas08@misena.edu.co>
2021-06-11 12:56:50 -0500

    Correciones en bootstrap.min.css

--- dashboard.php
+++ dashboard.php
@@ -34,7 +34,7 @@
                 <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
                     <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
                         <form class="form-inline d-none d-sm-inline-block mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                            <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ...">
+                            <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Buscar tabla">
                                 <div class="input-group-append"><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
                             </div>
                         </form>
